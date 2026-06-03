5f122cccd7d1d845e09ccfb14f511563074fb5bc
WiZzArD <arujaiswal12@gmail.com>
2026-06-03 23:08:15 +0530

    update database

--- aniverse-ai/server/src/config/db.ts
+++ aniverse-ai/server/src/config/db.ts
@@ -3,9 +3,9 @@ import prisma from "./prisma";
 export const connectDB = async () => {
   try {
     await prisma.$connect();
-    console.log("✅ Database Connected");
+    console.log("Database Connected");
   } catch (error) {
-    console.error("❌ Database Connection Failed", error);
+    console.error("Database Connection Failed", error);
     process.exit(1);
   }
 };
